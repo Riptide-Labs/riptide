351967c31cdeb408a043a33699a1b6ce93e5a7fe
Dustin Frisch <fooker@lab.sh>
2023-11-12 17:14:56 +0100

    chg: add locality enricher

---
+++ src/main/java/org/riptide/locality/LocalityEnricher.java
@@ -0,0 +1,47 @@
+package org.riptide.locality;
+
+import org.riptide.pipeline.EnrichedFlow;
+import org.riptide.pipeline.Enricher;
+import org.riptide.pipeline.Source;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class LocalityEnricher extends Enricher.Single {
+
+    @Override
+    protected CompletableFuture<Void> enrich(final Source source, final EnrichedFlow flow) {
+        if (flow.getSrcAddr() != null) {
+            flow.setSrcLocality(isPrivateAddress(flow.getSrcAddr())
+                    ? EnrichedFlow.Locality.PRIVATE
+                    : EnrichedFlow.Locality.PUBLIC);
+        }
+
+        if (flow.getDstAddr() != null) {
+            flow.setDstLocality(isPrivateAddress(flow.getDstAddr())
+                    ? EnrichedFlow.Locality.PRIVATE
+                    : EnrichedFlow.Locality.PUBLIC);
+        }
+
+        if (EnrichedFlow.Locality.PUBLIC.equals(flow.getDstLocality()) || EnrichedFlow.Locality.PUBLIC.equals(flow.getSrcLocality())) {
+            flow.setFlowLocality(EnrichedFlow.Locality.PUBLIC);
+        } else if (EnrichedFlow.Locality.PRIVATE.equals(flow.getDstLocality()) || EnrichedFlow.Locality.PRIVATE.equals(flow.getSrcLocality())) {
+            flow.setFlowLocality(EnrichedFlow.Locality.PRIVATE);
+        }
+
+        return null;
+    }
+
+    private static boolean isPrivateAddress(final InetAddress inetAddress) {
+        return inetAddress.isLoopbackAddress()
+                || inetAddress.isLinkLocalAddress()
+                || inetAddress.isSiteLocalAddress()
+                || inetAddress.isAnyLocalAddress()
+                || inetAddress.isMCSiteLocal()
+                || inetAddress.isMCLinkLocal()
+                || inetAddress.isMCNodeLocal()
+                || inetAddress.isMCOrgLocal();
+    }
+}
