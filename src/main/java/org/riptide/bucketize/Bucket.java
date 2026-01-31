848a495569249b39a9a300f25285abdae7e98e8d
Dustin Frisch <fooker@lab.sh>
2026-01-31 13:41:47 +0100

    fix: Handle zero duration flows

--- src/main/java/org/riptide/bucketize/Bucket.java
+++ src/main/java/org/riptide/bucketize/Bucket.java
@@ -4,6 +4,7 @@ import com.google.common.math.LongMath;
 import lombok.Builder;
 import lombok.Value;
 import org.riptide.pipeline.EnrichedFlow;
+import org.riptide.utils.Durations;
 
 import java.math.RoundingMode;
 import java.time.Duration;
@@ -25,12 +26,27 @@ public class Bucket {
         if (!samplingInterval.isPositive()) {
             throw new IllegalArgumentException("Sampling interval must be positive");
         }
+
         if (flow.getDeltaSwitched().isAfter(flow.getLastSwitched())) {
             throw new IllegalArgumentException("Flow has negative duration");
         }
-        final var result = new TreeMap<Instant, Bucket>();
+
         final var firstBucket = LongMath.divide(flow.getDeltaSwitched().toEpochMilli(), samplingInterval.toMillis(), RoundingMode.FLOOR);
         final var lastBucket = LongMath.divide(flow.getLastSwitched().toEpochMilli(), samplingInterval.toMillis(), RoundingMode.CEILING);
+
+        final var flowDuration = Duration.between(flow.getDeltaSwitched(), flow.getLastSwitched());
+        if (flowDuration.isZero()) {
+            final var bucket = Instant.ofEpochMilli((firstBucket + 1) * samplingInterval.toMillis());
+            return Map.of(bucket, Bucket.builder()
+                    .duration(samplingInterval)
+                    .bucketTime(bucket)
+                    .packets(flow.getPackets())
+                    .bytes(flow.getBytes())
+                    .bytesPerSecond(flow.getBytes())
+                    .packetsPerSecond(flow.getPackets())
+                    .build());
+        }
+
         final var buckets = lastBucket - firstBucket;
         if (buckets == 1) {
             // Flow fits into single bucket
@@ -40,11 +56,12 @@ public class Bucket {
                     .bucketTime(bucket)
                     .packets(flow.getPackets())
                     .bytes(flow.getBytes())
-                    .bytesPerSecond(flow.getBytes() / (double) samplingInterval.toSeconds())
-                    .packetsPerSecond(flow.getPackets() / (double) samplingInterval.toSeconds())
+                    .bytesPerSecond(flow.getBytes() / Durations.seconds(samplingInterval))
+                    .packetsPerSecond(flow.getPackets() / Durations.seconds(samplingInterval))
                     .build());
         }
-        final var flowDuration = Duration.between(flow.getDeltaSwitched(), flow.getLastSwitched());
+
+        final var result = new TreeMap<Instant, Bucket>();
         for (int i = 0; i < buckets; i++) {
             final var bucketEnd = Instant.ofEpochMilli((firstBucket + i + 1) * samplingInterval.toMillis());
             final var bucketStart = bucketEnd.minus(samplingInterval);
@@ -53,22 +70,21 @@ public class Bucket {
             if (flow.getDeltaSwitched().isAfter(bucketStart)) {
                 // Flow starts in bucket, but ends later
                 final var part = Duration.between(flow.getDeltaSwitched(), bucketEnd);
-                proportion = (double) part.toMillis() / (double) flowDuration.toMillis();
-
+                proportion = Durations.seconds(part) / Durations.seconds(flowDuration);
             } else if (flow.getLastSwitched().isBefore(bucketEnd)) {
                 // Flow ends in bucket, but starts earlier
                 final var part = Duration.between(bucketStart, flow.getLastSwitched());
-                proportion = (double) part.toMillis() / (double) flowDuration.toMillis();
-
+                proportion = Durations.seconds(part) / Durations.seconds(flowDuration);
             } else {
                 // Flow expands over the whole bucket
-                proportion = (double) samplingInterval.toMillis() / (double) flowDuration.toMillis();
+                proportion = Durations.seconds(samplingInterval) / Durations.seconds(flowDuration);
             }
+
             result.put(bucketEnd, Bucket.builder().duration(samplingInterval)
                     .packets(flow.getPackets() * proportion)
                     .bytes(flow.getBytes() * proportion)
-                    .bytesPerSecond(flow.getBytes() * proportion / (double) samplingInterval.toSeconds())
-                    .packetsPerSecond(flow.getPackets() * proportion / (double) samplingInterval.toSeconds())
+                    .bytesPerSecond(flow.getBytes() * proportion / Durations.seconds(samplingInterval))
+                    .packetsPerSecond(flow.getPackets() * proportion / Durations.seconds(samplingInterval))
                     .bucketTime(bucketEnd)
                     .build());
         }
