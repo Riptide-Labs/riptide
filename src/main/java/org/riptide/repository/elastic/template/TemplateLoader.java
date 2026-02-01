0631e97be2425e391e4982c27881ffc8a7d13cc5
Ronny Trommer <ronny@no42.org>
2026-02-01 17:05:49 +0100

    refactor: Bye bye Elasticsearch
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- src/main/java/org/riptide/repository/elastic/template/TemplateLoader.java
+++
@@ -1,7 +0,0 @@
-package org.riptide.repository.elastic.template;
-
-import java.io.IOException;
-
-public interface TemplateLoader {
-    String load(Version serverVersion, String resource) throws IOException;
-}
