508a4c89c89e7e2562500b47a42843c402c7fa7d
Ronny Trommer <ronny@no42.org>
2026-02-01 15:35:05 +0100

    refactor: Remove PostgreSQL
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- src/main/java/org/riptide/RiptideApplication.java
+++ src/main/java/org/riptide/RiptideApplication.java
@@ -3,16 +3,11 @@ package org.riptide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.elasticsearch.autoconfigure.ElasticsearchClientAutoConfiguration;
 import org.springframework.boot.elasticsearch.autoconfigure.ElasticsearchRestClientAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-        ElasticsearchClientAutoConfiguration.class, ElasticsearchRestClientAutoConfiguration.class,
-        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
+        ElasticsearchClientAutoConfiguration.class, ElasticsearchRestClientAutoConfiguration.class
 })
 @ConfigurationPropertiesScan
 public class RiptideApplication {
